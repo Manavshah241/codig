34d8c70664401632697dc54ad216bd41adb4950b
Manavshah241 <17385@bpsdoha.edu.qa>
2025-07-04 18:29:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 st.set_page_config(page_title="HELLO WORLD" , page_icon="ChatGPT Image Mar 31, 2025, 04_09_22 PM.png",layout="wide")
-img = Image.open("ChatGPT Image Mar 31, 2025, 04_09_22 PM.png")
+
 # HEADER PART # ---
 with st.container():
     st.title("A student learning python")
@@ -24,7 +24,6 @@ with st.container():
 with st.container():
     st.write("---")
     image_column , text_coloumn = st.columns((1000,3000))
-    with image_column:
-        st.image(img)
+    
     with text_coloumn:
         st.markdown("<h1 style='font-size: 100px;'>SO HERE WE GO AGAIN </h1>", unsafe_allow_html=True)
