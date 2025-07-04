b5cc53a70e26227a381874e4d3464e9b172f5576
Manavshah241 <17385@bpsdoha.edu.qa>
2025-07-04 19:25:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
+
 
 
 
@@ -10,12 +10,7 @@ from streamlit_lottie import st_lottie
 st.set_page_config(page_title="HELLO WORLD" , page_icon="ChatGPT Image Mar 31, 2025, 04_09_22 PM.png",layout="wide")
 #-----ANIMATION-----
 
-def load_lottie(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-lottie_coding = load_lottie("https://lottie.host/239e4c5b-86f5-4ddb-840f-b4c7585e572a/oiad7aK7Tl.json")
+
 img = Image.open("ChatGPT Image Mar 31, 2025, 04_09_22 PM.png")
 # HEADER PART # ---
 with st.container():
@@ -29,8 +24,7 @@ with st.container():
     left_column , right_column  =st.columns(2)
     with left_column :
         st.header("WHT DO I DO ")
-    with right_column:
-        st_lottie(lottie_coding, height = 300 , key="coding")
+ 
 
 with st.container():
     st.write("---")
