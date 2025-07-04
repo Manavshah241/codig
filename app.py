c19c4df6a17ebe2f3cad998133e7fe51fa37af9f
Manavshah241 <17385@bpsdoha.edu.qa>
2025-07-04 18:28:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 st.set_page_config(page_title="HELLO WORLD" , page_icon="ChatGPT Image Mar 31, 2025, 04_09_22 PM.png",layout="wide")
-
+img = Image.open("ChatGPT Image Mar 31, 2025, 04_09_22 PM.png")
 # HEADER PART # ---
 with st.container():
     st.title("A student learning python")
